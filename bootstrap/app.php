c0a44a9953f9bf41db3d78a74a8ccd08ea5d8095
Christopher Kaster <me@atomicptr.de>
2024-03-21 12:40:42 +0100

    add option to trust all proxies (make the reverse proxy story easier)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        if (env('TRUST_ALL_PROXIES')) {
+            $middleware->trustProxies(at: '*');
+        }
         $middleware->append([
             HasValidApiKey::class,
         ]);
